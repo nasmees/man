3c80af037f4e7a3f75ae59138ed2ac9dc4a5972c
nasmees <nasmees@gmail.com>
2015-08-17 22:56:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<META HTTP-EQUIV="Refresh" CONTENT="2;URL=/asm/admin/index.php">
+<META HTTP-EQUIV="Refresh" CONTENT="2;URL=/admin/index.php">
 <title>Access denied</title>
 <link href="css/main.css" rel="stylesheet" type="text/css" />
 </head>
